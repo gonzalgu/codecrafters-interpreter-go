27d224735ef2965f6161a3978ee0305b8d7c8d63
gonzalgu <gadol.gonzalez@gmail.com>
2024-07-15 11:07:38 -0400

    scanning parentheses

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,9 +5,163 @@ import (
 	"os"
 )
 
+type TokenType int
+
+const (
+	LEFT_PAREN TokenType = iota
+	RIGHT_PAREN
+	LEFT_BRACE
+	RIGHT_BRACE
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+	//One or two character tokens.
+	BANG
+	BANG_EQUAL
+	EQUAL
+	EQUAL_EQUAL
+	GREATER
+	GREATER_EQUAL
+	LESS
+	LESS_EQUAL
+	//Literals.
+	IDENTIFIER
+	STRING
+	NUMBER
+	//Keywords.
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+	EOF
+)
+
+type Token struct {
+	tokenType TokenType
+	lexeme    string
+	line      int
+}
+
+func (t TokenType) String() string {
+	return [...]string{
+		"LEFT_PAREN",
+		"RIGHT_PAREN",
+		"LEFT_BRACE",
+		"RIGHT_BRACE",
+		"COMMA",
+		"DOT",
+		"MINUS",
+		"PLUS",
+		"SEMICOLON",
+		"SLASH",
+		"STAR",
+		//One or two character tokens.
+		"BANG",
+		"BANG_EQUAL",
+		"EQUAL",
+		"EQUAL_EQUAL",
+		"GREATER",
+		"GREATER_EQUAL",
+		"LESS",
+		"LESS_EQUAL",
+		//Literals.
+		"IDENTIFIER",
+		"STRING",
+		"NUMBER",
+		//Keywords.
+		"AND",
+		"CLASS",
+		"ELSE",
+		"FALSE",
+		"FUN",
+		"FOR",
+		"IF",
+		"NIL",
+		"OR",
+		"PRINT",
+		"RETURN",
+		"SUPER",
+		"THIS",
+		"TRUE",
+		"VAR",
+		"WHILE",
+		"EOF",
+	}[t]
+}
+
+type Scanner struct {
+	source  []byte
+	tokens  []Token
+	start   int
+	current int
+	line    int
+}
+
+func NewScanner(source []byte) Scanner {
+	return Scanner{
+		source,
+		[]Token{},
+		0,
+		0,
+		1,
+	}
+}
+
+func (s *Scanner) ScanToks() []Token {
+	for _, c := range string(s.source) {
+		//fmt.Printf("rune %d: %v\n", i, c)
+		switch c {
+		case '(':
+			s.addToken(LEFT_PAREN)
+
+		case ')':
+			s.addToken(RIGHT_PAREN)
+
+		case ' ':
+			fallthrough
+		case '\r':
+			fallthrough
+		case '\t':
+			//do nothing
+		case '\n':
+			s.line++
+
+		default:
+			panic("Unexpected character")
+		}
+	}
+	s.addToken(EOF)
+	return s.tokens
+}
+
+func (s *Scanner) addToken(tokenType TokenType) {
+	text := string(s.source[s.start:s.current])
+	s.tokens = append(s.tokens, Token{
+		tokenType,
+		text,
+		s.line,
+	})
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
+	//fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
@@ -31,7 +185,12 @@ func main() {
 	}
 
 	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
+		scanner := NewScanner(fileContents)
+		tokens := scanner.ScanToks()
+		//fmt.Printf("len tokens = %d\n", len(tokens))
+		for _, tok := range tokens {
+			fmt.Printf("%v\n", tok.tokenType)
+		}
 	} else {
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}
